1f2dac3aecd9e9f9697875bcfc4d309788781550
VivatRafa <nazar.vasheniak@gmail.com>
2018-02-08 02:26:51 +0300

    server

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const repositories = require('./repositories')(db);
 const services = require('./services')(repositories);
 const app = require('./http/app')(services);
 const signals = require('./signals');
+const moment = require('moment');
+const TelegramBot = require('node-telegram-bot-api');
+const createHandler = require('node-github-webhook');
+const token = '536984728:AAEgy-XZQUoDGaTJWJ_WyeIm6ODykTeWkUo';
 
 const server = app.listen(port, () => {
     logger.info(`Listening on *:${port}`);
@@ -15,6 +19,14 @@ const shutdown = signals.init(async() => {
     await db.close();
     await server.close();
 });
+
+let bot = new TelegramBot(token, { polling: true });
+let handler = createHandler([ // multiple handlers 
+    { path: '/webhook', secret: '96beb5bd6c8facaf459cf9fd49deac3f' }
+]);
+
+let testdata = server.get('user');
+logger.info(testdata);
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
