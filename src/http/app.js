8e41f55db7e4505be314e703955d5d3df0d3da58
VivatRafa <nazar.vasheniak@gmail.com>
2018-02-08 01:41:52 +0300

    base

--- src/http/app.js
+++ src/http/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoute = require('./routes/user');
+const userRoute = require('./routes/project');
 const errorRoute = require('./routes/error');
 
 const app = express();
@@ -8,7 +9,11 @@ app.use(bodyParser.json());
 
 module.exports = (services) => {
     const user = userRoute.create(services);
+    const project = projectRoute.create(services);
+
     app.use('/users', user);
+    app.use('/projects', project);
     app.use(errorRoute);
+
     return app;
 };
