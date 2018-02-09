aa657cbfcacc8633ae922d4e1da3d43c5b5208db
VivatRafa <nazar.vasheniak@gmail.com>
2018-02-09 14:35:41 +0300

    base

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
-const logger = require('./libs/logger');
-const { port } = require('./configuration');
-const db = require('./database');
-const repositories = require('./repositories')(db);
-const services = require('./services')(repositories);
-const app = require('./http/app')(services);
-const signals = require('./signals');
+const logger = require('../libs/logger');
+const { port } = require('../configuration');
+const db = require('../database');
+const repositories = require('../repositories')(db);
+const services = require('../services')(repositories);
+const app = require('../http/app')(services);
+const signals = require('../signals');
 const moment = require('moment');
 const TelegramBot = require('node-telegram-bot-api');
 const createHandler = require('node-github-webhook');
 const token = '536984728:AAEgy-XZQUoDGaTJWJ_WyeIm6ODykTeWkUo';
+
+const bot = new TelegramBot(token, { polling: true });
+const handler = createHandler([ // multiple handlers 
+    { path: '/webhook', secret: '60f15027f1bb1e163691410845e34957814ef3ca' }
+]);
 
 const server = app.listen(port, () => {
     logger.info(`Listening on *:${port}`);
@@ -19,14 +24,6 @@ const shutdown = signals.init(async() => {
     await db.close();
     await server.close();
 });
-
-let bot = new TelegramBot(token, { polling: true });
-let handler = createHandler([ // multiple handlers 
-    { path: '/webhook', secret: '96beb5bd6c8facaf459cf9fd49deac3f' }
-]);
-
-let testdata = server.get('user');
-logger.info(testdata);
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
