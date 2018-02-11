ffc3c3cffba7d8f9618adbf10d9bc1ff2abce27a
VivatRafa <nazar.vasheniak@gmail.com>
2018-02-11 17:42:02 +0300

    1

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,6 @@
 const config = {
     connectionString: process.env.DATABASE_CONNECTION_STRING || 'mysql://root:Paravozik123@localhost:3306/codereview',
-    port: process.env.PORT || 8081,
+    port: process.env.PORT || 6000,
 };
 
 module.exports = config;
